20c2a00509ae3217b288f630e194fab56add395e
Caio Pantoja <caiopantoja1@hotmail.com>
2025-05-21 20:02:09 -0300

    Configurações iniciais JSP

--- src/main/java/br/com/fiap/service/UserService.java
+++ src/main/java/br/com/fiap/service/UserService.java
@@ -8,41 +8,13 @@ public class UserService {
 
     private static final UserDao userDao = new UserDao();
 
-    public static User createUser(Scanner scanner) {
-        System.out.print("Enter your Name: ");
-        String name = scanner.nextLine();
-
-        System.out.print("Enter your Email: ");
-        String email = scanner.nextLine();
-
-        System.out.print("Enter your Password: ");
-        String password = scanner.nextLine();
-
-        System.out.print("Enter your Number: ");
-        String phone = scanner.nextLine();
-
-        System.out.print("Enter your address: ");
-        String address = scanner.nextLine();
-
+    public User createUser(String name, String email, String password, String phone, String address) {
         User user = new User(0, name, email, password, phone, address);
         userDao.save(user);
         return user;
     }
 
-    public static User loginUser(Scanner scanner) {
-        System.out.print("Email: ");
-        String email = scanner.nextLine();
-
-        System.out.print("Password: ");
-        String password = scanner.nextLine();
-
-        User user = userDao.findByEmailAndPassword(email, password);
-        if (user != null) {
-            System.out.println("Login successful, Welcome " + user.getName());
-        } else {
-            System.out.println("Email or password incorrect.");
-        }
-
-        return user;
+    public User loginUser(String email, String password) {
+        return userDao.findByEmailAndPassword(email, password);
     }
 }
